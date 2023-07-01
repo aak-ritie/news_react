d14f56db2d0be5a831464b983c4d04a153d014f4
aak-ritie <aakritibhusal234@gmail.com>
2023-07-01 12:14:04 +0545

    SampleOutput Included

--- src/component/Item.js
+++ src/component/Item.js
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 
 export class Item extends Component {
   render() {
-    let { title, description, img_url } = this.props; //if props is an object then tyo object bta yo duita pull garera title ra description ko place ma provide garxa.This is called destructuring
+    let { title, description, img_url, newsUrl } = this.props; //if props is an object then tyo object bta yo duita pull garera title ra description ko place ma provide garxa.This is called destructuring
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
           <img src={img_url} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title} </h5>
-            <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-sm btn-primary">
+            <h5 className="card-title">{title}... </h5>
+            <p className="card-text">{description}...</p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
               Read More
             </a>
           </div>
