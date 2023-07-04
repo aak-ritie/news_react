6a9571b1b4bd5d3fe717903d9c4a4362bd35e4d8
aak-ritie <aakritibhusal234@gmail.com>
2023-07-04 16:11:59 +0545

    Till Video 39

--- src/component/Item.js
+++ src/component/Item.js
@@ -1,46 +1,43 @@
-import React, { Component } from "react";
+import React from "react";
 
-export class Item extends Component {
-  render() {
-    let { title, description, img_url, newsUrl, author, date, source } =
-      this.props; //if props is an object then tyo object bta yo duita pull garera title ra description ko place ma provide garxa.This is called destructuring
-    return (
-      <div className="my-3">
-        <div className="card">
-          <img
-            src={
-              !img_url
-                ? "https://images.hindustantimes.com/tech/img/2023/06/30/1600x900/World_Asteroid_Day_2023_1688098248538_1688098248723.jpg"
-                : img_url
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">
-              {title}
-              <span className="badge bg-secondary">{source}</span>
-            </h5>
-            <p className="card-text">{description}...</p>
-            <p className="card-text">
-              <small className="text-muted">
-                By {author ? author : "Unknown author"} on
-                {new Date(date).toGMTString()}
-              </small>
-            </p>
-            <a
-              rel="noreferrer"
-              href={newsUrl}
-              target="_blank"
-              className="btn btn-sm btn-dark"
-            >
-              Read More
-            </a>
-          </div>
+const Item = (props) => {
+  let { title, description, img_url, newsUrl, author, date, source } = props; //if props is an object then tyo object bta yo duita pull garera title ra description ko place ma provide garxa.This is called destructuring
+  return (
+    <div className="my-3">
+      <div className="card">
+        <img
+          src={
+            !img_url
+              ? "https://images.hindustantimes.com/tech/img/2023/06/30/1600x900/World_Asteroid_Day_2023_1688098248538_1688098248723.jpg"
+              : img_url
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">
+            {title}
+            <span className="badge bg-secondary">{source}</span>
+          </h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text">
+            <small className="text-muted">
+              By {author ? author : "Unknown author"} on
+              {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            rel="noreferrer"
+            href={newsUrl}
+            target="_blank"
+            className="btn btn-sm btn-dark"
+          >
+            Read More
+          </a>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Item;
--- src/component/News.js
+++ src/component/News.js
@@ -1,132 +1,96 @@
-import React, { Component } from "react";
-import Item from "./Item";
+import React, { useEffect, useState } from "react";
+
+import NewsItem from "./Item";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-export class News extends Component {
-  static defaultProps = {
-    country: "in",
-    pageSize: 8,
-    category: "general",
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   };
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  };
-  constructor() {
-    //constuctor will run only if tyo class ko object banxa
-    super();
-    this.state = {
-      articles: [],
-      loading: false,
-      page: 1,
-    };
-  }
-  async updateNews() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=6dfe677502db41c4a49e81539cf3b009&pagesize=${this.props.pageSize}`;
-    this.setState({ loading: true });
+
+  const updateNews = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=8b49c2cd16df4ff18e97557a2564eae0&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false,
-    });
-  }
-  async componentDidMount() {
-    this.updateNews();
-  }
-  handlePrevClick = async () => {
-    // let url = `https://newsapi.org/v2/top-headlines?country=${
-    //   this.props.country
-    // }&category=${
-    //   this.props.category
-    // }&apiKey=6dfe677502db41c4a49e81539cf3b009&page=${
-    //   this.state.page - 1
-    // }&pagesize=${this.props.pageSize}`;
-    // this.setState({ loading: true });
-    // let data = await fetch(url);
-    // let parsedData = await data.json();
-    // this.setState({
-    //   page: this.state.page - 1,
-    //   articles: parsedData.articles,
-    //   loading: false,
-    // });
-    this.setState({ page: this.state.page - 1 });
-    this.updateNews();
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
   };
-  handleNextClick = async () => {
-    // let url = `https://newsapi.org/v2/top-headlines?country=${
-    //   this.props.country
-    // }&category=${
-    //   this.props.category
-    // }&apiKey=6dfe677502db41c4a49e81539cf3b009&page=${
-    //   this.state.page + 1
-    // }&pagesize=${this.props.pageSize}`;
-    // this.setState({ loading: true });
-    // let data = await fetch(url);
-    // let parsedData = await data.json();
-    // this.setState({
-    //   page: this.state.page + 1,
-    //   articles: parsedData.articles,
-    //   loading: false,
-    // });
-    this.setState({ page: this.state.page + 1 });
-    this.updateNews();
+  useEffect(() => {
+    updateNews();
+  }, []); // jun kura run vayec vitra ko function which is effect run vaos vanni kura lekhinxa yo big bracket ma
+
+  // const handlePrevClick = async () => {
+  //   setPage(page - 1);
+  //   this.updateNews();
+  // };
+
+  // const handleNextClick = async () => {
+  //   setPage(page + 1);
+  //   this.updateNews();
+  // };
+
+  const fetchMoreData = async () => {
+    setPage(page + 1);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=8b49c2cd16df4ff18e97557a2564eae0&page=${page}&pageSize=${props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
   };
 
-  render() {
-    return (
-      <div className="container my-3">
-        <h1 className="text-center">NewsMonkey-Top Headlines</h1>
-        {this.state.loading && <Spinner></Spinner>}
-        <div className="row">
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
+  return (
+    <>
+      <h1 className="text-center" style={{ margin: "35px 0px" }}>
+        NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines
+      </h1>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row">
+            {articles.map((element) => {
               return (
                 <div className="col-md-4" key={element.url}>
-                  <Item
-                    title={element.title ? element.title.slice(0, 45) : ""} //element ma xa sabai title, description author etc sample output ma vako sabai but ahile ta api o through garexam but bujhna ko lagi sampleout ma vako json xa yesma
-                    description={
-                      element.description
-                        ? element.description.slice(0, 88)
-                        : ""
-                    }
-                    source={element.source.name}
+                  <NewsItem
+                    title={element.title ? element.title : ""}
+                    description={element.description ? element.description : ""}
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
                     author={element.author}
                     date={element.publishedAt}
-                    img_url={element.urlToImage}
-                    newsUrl={element.url}
-                  ></Item>
+                    source={element.source.name}
+                  />
                 </div>
               );
             })}
-          <div className="container d-flex justify-content-between">
-            <button
-              disabled={this.state.page <= 1}
-              type="button"
-              className="btn btn-dark"
-              onClick={this.handlePrevClick}
-            >
-              &larr; Previous
-            </button>
-            <button
-              disabled={
-                this.state.page + 1 >
-                Math.ceil(this.state.totalResults / this.props.pageSize)
-              }
-              type="button"
-              className="btn btn-dark"
-              onClick={this.handleNextClick}
-            >
-              Next &rarr;
-            </button>
           </div>
         </div>
-      </div>
-    );
-  }
-}
+      </InfiniteScroll>
+    </>
+  );
+};
+News.defaultProps = {
+  country: "in",
+  pageSize: 8,
+  category: "general",
+};
 
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
 export default News;
